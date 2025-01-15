c3270999b01a3a4074f04ec83925f98c9108de18
Mahhesh-Thakor <mahesh.thakor@arsenaltech.com>
2025-01-15 19:32:51 +0530

    Removed Local Server from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        host: 'react-app.local', // Set the hostname to your custom domain
-        port: 5173, // Default port is 5173
-    },
+    // server: {
+    //     host: 'react-app.local', // Set the hostname to your custom domain
+    //     port: 5173, // Default port is 5173
+    // },
 });
